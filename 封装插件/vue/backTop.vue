637e4db71af3fd31383132e785e41ac0c0f46d44
yx <huangyx07@126.com>
2020-07-19 11:02:32 +0800

    整理之前封装好的组件

---
+++ 封装插件/vue/backTop.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="backtop">
+    <img
+      src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=43586916,287019524&fm=26&gp=0.jpg"
+      alt
+    />
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  },
+}
+</script>
+<style lang="less" scoped>
+.backtop {
+  position: fixed;
+  bottom: 50px;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ 封装插件/vue/顶部navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="navbar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.navbar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.1);
+
+  /* position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99; */
+
+  .left,
+  .right {
+    width: 60px;
+  }
+  .center {
+    flex: 1;
+    text-align: center;
+  }
+}
+</style>
